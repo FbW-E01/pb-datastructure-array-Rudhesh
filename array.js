786f54795f2d977c8e04b81ab80f1a546aae42f9
dci <rudeshdhote@gmail.com>
2021-04-13 12:00:11 +0200

    initial commit

---
+++ array.js
@@ -0,0 +1,12 @@
+const myArray = ["potato", "tomato", "onion", "garlic", "ginger"];
+
+
+
+myArray[2] = "thirdElement"
+
+console.log(myArray[2]);
+
+myArray[3] = "fourth element"
+
+console.log(myArray);
+
